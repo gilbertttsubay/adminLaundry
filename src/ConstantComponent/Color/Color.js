60449f7a30f3d15948354af712a1be9fd570abbb
GILBERT <gilbertttsubay@gmail.com>
2020-10-04 16:18:16 +0700

    add constant color

---
+++ src/ConstantComponent/Color/Color.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#fddb3a",
+  accent: "#f6f4e6",
+  accent2: "#52575d",
+};
---
+++ src/ConstantComponent/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./header.css";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="allContainer">
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Header;
