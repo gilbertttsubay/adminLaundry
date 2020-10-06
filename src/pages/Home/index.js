9298effbb0184207ac67e041910dc5b2f3b66acf
dhioputro <pramadhio.ari@gmail.com>
2020-10-06 09:15:30 +0700

    add bootstrap tab, still has issues

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
-
-import Tabs from '../../ConstantComponent/Tabs';
-
+import { Tabs, Tab } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            activeTab: "incoming"
+        }
+
+        this.handleTab = this.handleTab.bind(this);
+    }
+
+    handleTab(selectedTab) {
+
+        this.setState({
+          activeTab: selectedTab
+        });
+      }
 
     render() { 
         return ( 
             <div>
-                <h1>Home</h1>
-                {/* <Tabs> 
-                    <div label="Incoming Order"> 
+                <Tabs       
+                    id="home-tab"
+                    activeKey={this.state.activeTab}
+                    onSelect={this.handleTab}
+                >
+                    <Tab eventKey="incoming" title="Incoming Order">
                         Incoming Order
-                    </div> 
-                    <div label="Ongoing Order"> 
-                        Ongoing Order 
-                    </div> 
-                    <div label="Order History"> 
+                    </Tab>    
+                    <Tab eventKey="ongoing" title="Ongoing Order">
+                        Ongoing Order
+                    </Tab>    
+                    <Tab eventKey="history" title="Order History">
                         Order History
-                    </div> 
-                    <div label="User Page"> 
-                        User Page
-                    </div> 
-                </Tabs>  */}
+                    </Tab>    
+                    <Tab eventKey="users" title="Customer List">
+                        User Lists
+                    </Tab>    
+
+                </Tabs>
             </div>    
         );
     }
 }
+
  
 export default Home;
