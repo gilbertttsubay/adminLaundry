5d48d37944fc103bf5f0c8c892a0667cd8e0bdbb
dhioputro <pramadhio.ari@gmail.com>
2021-02-11 15:06:20 +0700

    Updates

---
+++ precache-manifest.a487a1434f1a0bfd3bda76f7633730ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8b6c4bc0a4db844e9ab6fc8097ce8f",
+    "url": "/adminLaundryReact/index.html"
+  },
+  {
+    "revision": "0d24f911c2dbf8a09d59",
+    "url": "/adminLaundryReact/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "971c33c08b3e923cd23b",
+    "url": "/adminLaundryReact/static/css/main.abea8ff3.chunk.css"
+  },
+  {
+    "revision": "0d24f911c2dbf8a09d59",
+    "url": "/adminLaundryReact/static/js/2.daf012f0.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/adminLaundryReact/static/js/2.daf012f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971c33c08b3e923cd23b",
+    "url": "/adminLaundryReact/static/js/main.1dbf3876.chunk.js"
+  },
+  {
+    "revision": "50a3bcf9a4e7a15d1fc9",
+    "url": "/adminLaundryReact/static/js/runtime-main.2ed7c8e0.js"
+  },
+  {
+    "revision": "69029720e3414042791cc655513108e1",
+    "url": "/adminLaundryReact/static/media/logo.69029720.png"
+  }
+]);
